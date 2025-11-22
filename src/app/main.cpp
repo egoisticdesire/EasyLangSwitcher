e13c335871ca6cac1d1f0e12a962b61926dabffd
egoistic_desire <egoistic.desire@gmail.com>
2025-11-23 01:23:47 +0400

    upd main module

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,21 +1,28 @@
 #include "../core/handlers/kb.h"
 #include "../ui/helpers/iconHelper.h"
 #include "../ui/tray/tray.h"
+#include "../core/config/app_settings.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[]) {
     const QApplication app(argc, argv);
     QApplication::setQuitOnLastWindowClosed(false);
     QApplication::setWindowIcon(IconHelper::loadIcon(":/icons/icons/FlashSparkleFilled2.png"));
 
+    AppSettings::load();
+    qDebug() << "main: loaded settings -> hotkey:" << AppSettings::hotkeyName << "vk=" << AppSettings::hotkeyMainVk;
+
     TrayManager tray;
     tray.show();
 
     KeyboardHandler kbHandler;
     kbHandler.start();
+    qDebug() << "main: KeyboardHandler started.";
 
     QObject::connect(&app, &QApplication::aboutToQuit, [&]() {
         kbHandler.stop();
+        qDebug() << "main: KeyboardHandler stopped on exit.";
     });
     QObject::connect(&tray, &TrayManager::exitRequested, [&]() {
         QApplication::quit();
